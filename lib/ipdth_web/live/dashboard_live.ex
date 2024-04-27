9457aad3c253a468560495be2f979e9263eacd42
Marcus Autenrieth <myself@prakti.org>
2024-04-27 17:17:36 +0200

    💄 beautify the users table

--- lib/ipdth_web/live/dashboard_live.ex
+++ lib/ipdth_web/live/dashboard_live.ex
@@ -10,6 +10,29 @@ defmodule IpdthWeb.DashboardLive do
   def render(assigns) do
     ~H"""
     <%= if @current_user do %>
+      <div class="p-5 grid grid-cols-4 gap-4">
+        <.badge color="gray"> Gray </.badge>
+        <.badge color="zinc"> Zinc </.badge>
+        <.badge color="neutral"> Neutral </.badge>
+        <.badge color="stone"> Stone </.badge>
+        <.badge color="red"> Red </.badge>
+        <.badge color="orange"> Orange </.badge>
+        <.badge color="amber"> Amber </.badge>
+        <.badge color="yellow"> Yellow </.badge>
+        <.badge color="lime"> Lime </.badge>
+        <.badge color="green"> Green </.badge>
+        <.badge color="emerald"> Emerald </.badge>
+        <.badge color="teal"> Teal </.badge>
+        <.badge color="cyan"> Cyan </.badge>
+        <.badge color="sky"> Sky </.badge>
+        <.badge color="blue"> Blue </.badge>
+        <.badge color="indigo"> Indigo </.badge>
+        <.badge color="violet"> Violet </.badge>
+        <.badge color="purple"> Purple </.badge>
+        <.badge color="fuchsia"> Fuchsia </.badge>
+        <.badge color="pink"> Pink </.badge>
+        <.badge color="rose"> Rose </.badge>
+      </div>
       <h2>This is the user dashboard for <%= @current_user.email %></h2>
       <div class="p-5 grid grid-cols-2 gap-4">
         <div class="relative rounded ring-1 ring-zinc-300 border-b-2 border-zinc-300 shadow-md p-2">
@@ -19,7 +42,7 @@ defmodule IpdthWeb.DashboardLive do
             this is a description lorem ipsum dolor sit amet blah foo blah 1 2 3 4 5
           </p>
 
-          <div class="absolute bottom-2 right-2 bg-green-200 rounded-full px-3 py-2 text-xs font-bold text-green-700 leading-none ring-1 ring-inset ring-green-500/40">
+          <div class="absolute bottom-2 right-2 bg-green-200 rounded-full px-2.5 py-1.5 text-xs font-bold text-green-700 leading-none ring-1 ring-inset ring-green-500/40">
             Active
           </div>
         </div>
